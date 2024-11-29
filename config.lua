db9cbb461218a1dab3742169da5dcb84d977f79b
VerstandTsai <hackertsaigenz@gmail.com>
2024-11-29 16:50:09 +0800

    Enabled tree-sitter and added catppuccin integrations

--- config.lua
+++ config.lua
@@ -23,10 +23,32 @@ vim.api.nvim_create_autocmd("BufEnter", {
     end
 })
 
+-- nvim-treesitter highlighting
+require'nvim-treesitter.configs'.setup {
+    highlight = {
+        enable = true,
+        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+        -- Using this option may slow down your editor, and you may see some duplicate highlights.
+        -- Instead of true it can also be a list of languages
+        additional_vim_regex_highlighting = false,
+    },
+}
+
 -- Catppuccin theme
 require("catppuccin").setup({
     flavour = "macchiato", -- latte, frappe, macchiato, mocha
-    transparent_background = true -- disables setting the background color.
+    transparent_background = true, -- disables setting the background color.
+    integrations = {
+        coc_nvim = true,
+        nvimtree = true,
+        treesitter = true,
+        indent_blankline = {
+            enabled = true,
+            scope_color = "", -- catppuccin color (eg. `lavender`) Default: text
+            colored_indent_levels = true,
+        },
+    }
 })
 
 -- setup must be called before loading
